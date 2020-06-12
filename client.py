fdc3006e61d228ec8b0a495ea55c0939bd83d8ce
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2020-06-12 00:00:38 -0300

    altas mudança ae

--- client.py
+++ client.py
@@ -1,57 +1,95 @@
 import socket
 from random import randint
 
+
 class Client:
+
     def __init__(self, player_id):
         self.player_id = player_id
+        self.cliSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.pontuacao = 0
+        self.vidas = 5
 
     def __str__(self):
         return self.player_id
 
-    def client(self):
+    def conectar(self):
         HOST = '127.0.0.1'  # The server's hostname or IP address
-        PORT = 65432        # The port used by the server
+        PORT = 65432  # The port used by the server
 
-        cliSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        cliSocket.connect((HOST, PORT))
-        cliSocket.send('TESTE'.encode('UTF-8'))
-        data = cliSocket.recv(1024).decode('UTF-8')
-        cliSocket.close()
+        self.cliSocket.connect((HOST, PORT))
+
+    def fechar(self):
+        self.cliSocket.close()
+
+    def receber(self):
+        data = self.cliSocket.recv(1024).decode('UTF-8')
         return data
 
-jogo= Client('1')
-print(jogo)
-pontuação = 0
-tentativa = 0
+    def enviar(self, data):
+        self.cliSocket.send(data.encode('UTF-8'))
+
+
+cliente = Client('1')
+print(cliente)
+
+cliente.conectar()
 
 while True:
-    num1 = int(jogo.client())
-    num2 = int(jogo.client())
-    operacao = str(randint(0,4))
-    if operacao == "0":
-        resp = input('Quanto é: \t' + str(num1) + ' + ' + str(num2) + ': ') 
-        if int(resp) == num1+num2:
-            pontuação += 1
-        else:
-            tentativa += 1
-    elif operacao == "1":
-        resp = input('Quanto é: \t' + str(num1) + ' - ' + str(num2) + ': ') 
-        if int(resp) == num1-num2:
-            pontuação += 1
-        else:
-            tentativa += 1
-    else:
-        resp = input('Quanto é: \t' + str(num1) + ' x ' + str(num2) + ': ') 
-        if int(resp) == num1*num2:
-            pontuação += 1
-        else:
-            tentativa += 1
-    
-    if pontuação == 10:
-        print("Parabéns vc ganhou")
+    ready = input('Comando (READY ou CLOSE): ')
+    if ready == "READY":
+        cliente.enviar(ready)
+        while True:
+            data = cliente.receber()
+            num1 = int(data[0])
+            num2 = int(data[1])
+            operacao = int(data[2])
+            print(num1, num2, operacao)
+            if operacao == 0:
+                resp = input('Quanto é: \t' + str(num1) + ' + ' + str(num2) + ': ')
+                if int(resp) == num1 + num2:
+                    cliente.pontuacao += 1
+                else:
+                    cliente.vidas -= 1
+            elif operacao == 1:
+                resp = input('Quanto é: \t' + str(num1) + ' - ' + str(num2) + ': ')
+                if int(resp) == num1 - num2:
+                    cliente.pontuacao += 1
+                else:
+                    cliente.vidas -= 1
+            else:
+                resp = input('Quanto é: \t' + str(num1) + ' x ' + str(num2) + ': ')
+                if int(resp) == num1 * num2:
+                    cliente.pontuacao += 1
+                else:
+                    cliente.vidas -= 1
+
+            if cliente.vidas == 0:
+                cliente.enviar('LOST')
+                print('Precisa treinar mais :(')
+                break
+
+            if cliente.pontuacao == 10:
+                cliente.enviar('WIN')
+                print('Parabéns :)')
+                break
+
+            while True:
+                next = input('Comando (NEXT ou VLIFES ou VPOINTS): ')
+                if next == 'NEXT':
+                    cliente.enviar(next)
+                    break
+                elif next == 'VLIFES':
+                    print('Vidas: ', cliente.vidas)
+                elif next == 'VPOINTS':
+                    print('Pontos: ', cliente.pontuacao)
+                else:
+                    print('Comando inválido')
+
+    elif ready == 'CLOSE':
+        cliente.enviar(ready)
         break
     else:
-        continue
-    if (tentativa == 5):
-        print("Precisa treinar mais :(")
-        break
+        print('Comando Inválido')
+
+cliente.fechar()
--- server.py
+++ server.py
@@ -3,26 +3,47 @@
 import socket
 from random import randint
 
-def server():
-    HOST = '127.0.0.1'  
-    PORT = 65432        
 
+def server():
+    HOST = '127.0.0.1'
+    PORT = 65432
+
+    next = 'NEXT'
 
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     serverSocket.bind((HOST, PORT))
     serverSocket.listen(2)
+    print('Server rodando')
     while True:
-        (cliSocket, addr) = serverSocket.accept()  
-        print(f'Connected by', addr)   
+        (cliSocket, addr) = serverSocket.accept()
+        print(f'Connected by', addr)
         while True:
-            data = cliSocket.recv(1024).decode('UTF-8')
-            print('Recebido: ', data)
-            if not data:
+            ready = cliSocket.recv(1024).decode('UTF-8')
+            if not ready:
+                continue
+            print('Recebido: ', ready)
+            if ready == 'READY':
+                while True:
+                    if next == 'NEXT':
+                        num1 = str(randint(0, 9))
+                        num2 = str(randint(0, 9))
+                        operacao = str(randint(0, 2))
+                        print('Enviando ', num1, num2, operacao)
+                        cliSocket.send((num1 + num2 + operacao).encode('UTF-8'))
+                    elif next == 'GIVEUP':
+                        cliSocket.send('Precisa treinar mais :('.encode('UTF-8'))
+                        break
+                    elif next == 'LOST' or next == 'WIN':
+                        break
+                    next = cliSocket.recv(1024).decode('UTF-8')
+            elif ready == 'CLOSE':
                 break
-            msg = str(randint(0,9))
-            print('Enviando', msg)
-            cliSocket.send(msg.encode('UTF-8'))
+
+        # msg = str(randint(0, 9))
+        # print('Enviando', msg)
+        # cliSocket.send(msg.encode('UTF-8'))
         cliSocket.close()
+
 
 server()
